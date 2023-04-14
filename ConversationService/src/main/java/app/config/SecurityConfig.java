0337eae1936af94129aa18fc3591469324ae3bd2
kbarszczak <kamilbarszczak62@gmail.com>
2023-04-14 13:12:41 +0200

    Removed prometheus

--- ConversationService/src/main/java/app/config/SecurityConfig.java
+++ ConversationService/src/main/java/app/config/SecurityConfig.java
@@ -23,10 +23,6 @@ public class SecurityConfig {
         http
                 .csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/actuator*")
-                .permitAll()
-                .requestMatchers("/actuator/*")
-                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
